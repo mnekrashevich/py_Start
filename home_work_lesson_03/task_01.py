28f02e36d412d25827b02c00ce9b1b169bd29cc0
Михаил <mnekrashevich@gmail.com>
2021-07-23 08:19:46 +0300

    Первое задание выполнено

---
+++ home_work_lesson_03/task_01.py
@@ -0,0 +1,32 @@
+# coding=utf8
+
+# 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
+# Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+
+def division(divisible, divisor):
+    """
+    Функция division(divisible, divisor) принимает в качестве параметров делимое и делитель. Делает попытку
+    перобразовать их к float. Возвращает результат деления делимого на делитель. В случае деления на ноль или
+    невозможности преобразовать входные данные к числам возвращает строку с описанием ошибки.
+    Параметры:
+    divisible - Делимое
+    divisor - Делитель
+    Возвращаемое значение:
+    Числовое значение типа float, Строка (в случае ошибки).
+    """
+    try:
+        divisible_float = float(divisible)
+        divisor_float = float(divisor)
+        result = divisible_float/divisor_float
+    except ZeroDivisionError:
+        result = 'Ошибка! Деление на ноль!'
+    except Exception:
+        result = 'Ошибка! Неверные входные данные!'
+    finally:
+        return result
+
+
+for args in [(2, 4), (5, 0), (0, 7), ('asdf', 1), (1, 'asdf'), ('asdf', 'asdf'), ('12', '6'), (True, False), (False, True)]:
+    print(division(*args))
+
+print(division(input('Делимое :'), input('Делитель :')))
